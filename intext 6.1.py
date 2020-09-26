a20ef2f6e0a6af4764a19c075e6f0f481c9690ae
dougbrown1048 <70710191+dougbrown1048@users.noreply.github.com>
2020-09-26 16:53:09 -0400

    Add files via upload

---
+++ intext 6.1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def compare(x,y):
+    if x > y:
+        return 1
+    if x == y:
+        return 0
+    if x < y:
+        return -1
+    
+print(compare(3,2))
+
