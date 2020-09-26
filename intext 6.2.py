ba0f8cf0049b1f38041d05cf993e11e4eafa54cb
dougbrown1048 <70710191+dougbrown1048@users.noreply.github.com>
2020-09-26 16:55:41 -0400

    Add files via upload

---
+++ intext 6.2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 12:19:57 2020
+
+@author: dbrown
+"""
+
+import math
+
+def hypotenuse(l1,l2):
+    
+    l1squared = l1**2
+    l2squared = l2**2
+    sumSquares = l1squared + l2squared
+    length = math.sqrt(sumSquares)
+    return (0.0)
+
+hypotenuse(3,4)
+
+
+    
---
+++ intext 6.4.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 12:32:18 2020
+
+@author: dbrown
+"""
+
+def isBetween(x,y,z):
+    if x <= y and y <= z:
+        print("True")
+    else:
+        print("False")
+
+isBetween(2,3,4)
