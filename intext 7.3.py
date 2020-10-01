a9da7ec2abd6ed28bcaa32daa0656c590c04a69d
dougbrown1048 <70710191+dougbrown1048@users.noreply.github.com>
2020-10-01 13:06:48 -0400

    Add files via upload

---
+++ intext 7.3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 13:04:54 2020
+
+@author: dbrown
+"""
+
+def print_n(s,n):
+    while n > 0:
+        print(s)
+        n = n - 1
+
+print_n("test" , 5)
